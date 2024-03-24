1ab4ad2b41a064fd9bbcf694f41522e4cc5b44a2
Shem Karafa <karafashem@gmail.com>
2024-03-24 14:07:03 +0300

    create index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+const eventName = ["birthday"];
+
+function writeCards(names, eventName){
+    let messages = [];
+    for (let i = 0; 1 < names.legnth; i++) {
+
+        let mesage = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        messages.push(message);
+        debugger;
+    }
+    return messages;
+}
+  // Example usage:
+  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function countdown(num) {
+    while (num >=0) {
+        console.log(num);
+        num--;
+    }
+}
